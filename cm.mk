58ec89717bc3be43b2116b9aa5e892f5a69acf83
Andrey <microzans@gmail.com>
2016-11-13 21:41:49 +0300

    )
    )

--- cm.mk
+++ cm.mk
@@ -1,20 +1,5 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Specify phone tech before including full_phone
+dev_name := SandroidTeam
+tree := NIGHTLY
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Inherit some common CM stuff.
@@ -27,7 +12,7 @@ $(call inherit-product, $(LOCAL_PATH)/device_kanas.mk)
 PRODUCT_RELEASE_NAME := kanas
 
 # Custom unofficial build tag
-TARGET_UNOFFICIAL_BUILD_ID := SandroidTeam
+TARGET_UNOFFICIAL_BUILD_ID := $(dev_name)-$(tree)
 
 # Override build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
